6645682365479f4879c0319df07e1e7007175308
Steph T <wytseng@uci.edu>
2023-01-31 10:39:45 -0800

    created mongo db models for user, store, review (may subject to change)

--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,39 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
+const reviewSchema = require("./Review");
+const storeSchema = require('./Store');
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
+  username: {
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
+    trim: true,
   },
   password: {
     type: String,
     required: true,
     minlength: 5,
   },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  savedStores: [storeSchema],
+  reviews: [reviewSchema], 
 });
 
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
-
   next();
 });
 
@@ -32,4 +41,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-module.exports = model("User", userSchema);
+const User = model("User", userSchema);
+
+module.exports = User;
