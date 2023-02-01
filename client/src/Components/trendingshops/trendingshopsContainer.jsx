4964128546453c4e0e9055a1bbcf05bc1a16b4cf
JustinCChen <justinchen1211@gmail.com>
2023-02-01 10:15:07 -0800

    home page skeleton

---
+++ client/src/Components/trendingshops/trendingshopsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../favoritesList/favoritesList.css'
+import SingleItem from '../favoritesList/singleItem'
+
+const TrendingShopsContainer = () => {
+  return (
+    <div className=''>
+      <h2>Trending Shops</h2>
+      <SingleItem />
+    </div>
+  )
+}
+
+export default TrendingShopsContainer
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 import Navbar from "../../Components/navbar";
+import { Link } from 'react-router-dom';
+import SearchBar from "../../Components/searchBar/searchBar";
+import { Button, Checkbox, Form, Input } from "antd";
+import TrendingShopsContainer  from '../../Components/trendingshops/trendingshopsContainer'
+
 
 const Home = () => {
   useEffect(() => {
@@ -9,6 +14,17 @@ const Home = () => {
     <>
       <Navbar navItem="home" />
       <h1 className="text-blue-400">CommuniTEA</h1>
+      <div>
+        <SearchBar />
+        <Link to="/login">
+          <Button>LOGIN</Button>
+        </Link>
+        <h5>or</h5>
+        <Link to="/register">
+          <Button>SIGNUP</Button>
+        </Link>
+      </div>
+        <TrendingShopsContainer />
     </>
   );
 };
