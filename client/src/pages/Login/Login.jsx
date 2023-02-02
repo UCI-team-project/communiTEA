6a2b434c74a87bae2d24ec882d9755b9d408e887
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-02-02 09:20:30 -0800

    used error and data cases for login and signup, fixed from compile issues

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../utils/mutations";
-import { Button, Checkbox, Form, Input } from "antd";
+// import { Button, Checkbox, Form, Input } from "antd";
+import { Button } from "antd";
 import Auth from "../../utils/auth.js";
 import style from "./login.module.css";
 import { Link } from "react-router-dom";
@@ -43,42 +44,54 @@ export default function Login() {
         <Navbar navItem={"login"} />
       </div>
       <div className={style.container}>
-        <h1 className={style.title}>Login</h1>
-        <p className={style.text}>
-          Find the best boba in your area. Give them a review to help your
-          fellow boba-nians in finding the best boba in the area. 😉
-        </p>
-        <form className={style.formContainer} onSubmit={handleFormSubmit}>
-          <input
-            className={style.inputField}
-            type="text"
-            autoComplete="off"
-            name="name"
-            value={formState.name}
-            onChange={handleInputChange}
-            placeholder="Enter Username"
-          />
+        {data ? (
+          <p>
+            Success! You may now head <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <>
+            <h1 className={style.title}>Login</h1>
+            <p className={style.text}>
+              Find the best boba in your area. Give them a review to help your
+              fellow boba-nians in finding the best boba in the area. 😉
+            </p>
+            <form className={style.formContainer} onSubmit={handleFormSubmit}>
+              <input
+                className={style.inputField}
+                type="text"
+                autoComplete="off"
+                name="name"
+                value={formState.name}
+                onChange={handleInputChange}
+                placeholder="Enter Username"
+              />
 
-          <input
-            className={style.inputField}
-            type="password"
-            name="password"
-            value={formState.password}
-            onChange={handleInputChange}
-            placeholder="Password"
-          />
-          <span className={style.btnContainer}>
-            <Button className={style.loginBtn} htmlType='submit' block>
-              Login
-            </Button>
-          </span>
-          <div className={style.alternativeOptionSection}>
-            <h4 className={style.altEl}>Don't have an account yet?</h4>
-            <Link to='/signup'>
-              <Button className={style.signUpBtn}>Sign Up</Button>
-            </Link>
-          </div>
-        </form>
+              <input
+                className={style.inputField}
+                type="password"
+                name="password"
+                value={formState.password}
+                onChange={handleInputChange}
+                placeholder="Password"
+              />
+              <span className={style.btnContainer}>
+                <Button className={style.loginBtn} htmlType="submit" block>
+                  Login
+                </Button>
+              </span>
+              <div className={style.alternativeOptionSection}>
+                <h4 className={style.altEl}>Don't have an account yet?</h4>
+                <Link to="/signup">
+                  <Button className={style.signUpBtn}>Sign Up</Button>
+                </Link>
+              </div>
+            </form>
+          </>
+        )}
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
       </div>
       <FooterComponent />
     </>
--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -43,40 +43,52 @@ export default function Signup() {
         <Navbar />
       </div>
       <div className={style.container}>
-        <h1>Sign Up</h1>
-        <p className={style.text}>
-          Sign up now for more information on trending boba shops from some of
-          the greatest boba enthusiasts in your area! 🧋
-        </p>
-        <form className={style.formContainer} onSubmit={handleFormSubmit}>
-          <input
-            className={style.inputField}
-            type="text"
-            name="name"
-            value={formState.name}
-            onChange={handleChange}
-            placeholder="Enter Username"
-          />
-          <input
-            className={style.inputField}
-            type="password"
-            name="password"
-            value={formState.password}
-            onChange={handleChange}
-            placeholder="Password"
-          />
-          <span className={style.btnContainer}>
-            <Button className={style.signUpBtn} htmlType="submit" block>
-              Sign Up
-            </Button>
-          </span>
-          <div className={style.alternativeOptionSection}>
-            <h4 className={style.altEl}>Already have an account?</h4>
-            <Link to="/login">
-              <Button className={style.loginBtn}>Login</Button>
-            </Link>
-          </div>
-        </form>
+        {data ? (
+          <p>
+            Success! You may now head <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <>
+            <h1>Sign Up</h1>
+            <p className={style.text}>
+              Sign up now for more information on trending boba shops from some
+              of the greatest boba enthusiasts in your area! 🧋
+            </p>
+            <form className={style.formContainer} onSubmit={handleFormSubmit}>
+              <input
+                className={style.inputField}
+                type="text"
+                name="name"
+                value={formState.name}
+                onChange={handleChange}
+                placeholder="Enter Username"
+              />
+              <input
+                className={style.inputField}
+                type="password"
+                name="password"
+                value={formState.password}
+                onChange={handleChange}
+                placeholder="Password"
+              />
+              <span className={style.btnContainer}>
+                <Button className={style.signUpBtn} htmlType="submit" block>
+                  Sign Up
+                </Button>
+              </span>
+              <div className={style.alternativeOptionSection}>
+                <h4 className={style.altEl}>Already have an account?</h4>
+                <Link to="/login">
+                  <Button className={style.loginBtn}>Login</Button>
+                </Link>
+              </div>
+            </form>
+          </>
+        )}
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
       </div>
       <FooterComponent />
     </>
