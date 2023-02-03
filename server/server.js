fb9bfa8ac2c71cb2d1929209f3f0f309f7b4a539
Darius Garcia <dariusgarcia888@gmail.com>
2023-02-03 11:23:43 -0800

    added function to fetch stores in dashboard

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const { join } = require("path");
-const { authMiddleware } = require("./utils/auth.js");
-const axios = require("axios");
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+const { join } = require('path')
+const { authMiddleware } = require('./utils/auth.js')
+const axios = require('axios')
 
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config')
@@ -29,44 +29,45 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
 })
 
-// let config = {
-//   headers: {
-//     Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-//   },
-//   params: {
-//     term: "boba",
-//     location: "92683",
-//     radius: 10000,
-//     sort_by: "best_match",
-//     limit: 1,
-//   },
-// };
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer vS6Jx8XbtA3xq0cYn9s--SOFHtlDBRsgMu2b2GjLBMOnr2XvcTcoNTpFLcR6E9Gov1OTWlTXda6CprgUGD9hdmIyqFIUI3SHPsyTuwim7hfHy5NYCFJz-rI7yDl9Y3Yx',
+  },
+}
 
-// app.get("/api/yelp", async (req, res) => {
-//   const response = await axios
-//     .get("https://api.yelp.com/v3/businesses/search", config)
-//     .then((response) => {
-//       console.log(response.data);
-//       res.json(response.data);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// });
+app.get('/api/yelp', async (req, res) => {
+  const response = await axios
+    .get(
+      'https://api.yelp.com/v3/businesses/search?location=90703&term=boba&radius=1000&sort_by=best_match&limit=20',
+      options
+    )
+    .then((response) => {
+      console.log(response.data)
+      res.json(response.data)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+
+  response
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start()
   server.applyMiddleware({ app })
 
-  db.once("open", () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`
         \n------------------------------------------
         \nAPI server running on port ${PORT}!
         \nUse GraphQL at http://localhost:${PORT}${server.graphqlPath}
-        \n------------------------------------------`);
-    });
-  });
-};
+        \n------------------------------------------`)
+    })
+  })
+}
 
 startApolloServer(typeDefs, resolvers)
