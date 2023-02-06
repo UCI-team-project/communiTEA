cd28663cd7039d72676f24e78198ad0c70b39b5d
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-02-06 10:28:27 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "..", "client", "build")));
 }
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "client", "build", "index.html"));
 });
 
