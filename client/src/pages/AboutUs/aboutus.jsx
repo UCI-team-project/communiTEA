340e81cb6a23d5d4df9052db8a438232c586e802
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-02-05 22:56:56 -0800

    added johnathans about me

--- client/src/pages/AboutUs/aboutus.jsx
+++ client/src/pages/AboutUs/aboutus.jsx
@@ -1,13 +1,13 @@
-import { useEffect } from 'react'
-import style from './aboutus.css'
+import { useEffect } from "react";
+import style from "./aboutus.css";
 
-import HeaderComponent from '../../Components/header'
-import FooterComponent from '../../Components/footer/footer'
+import HeaderComponent from "../../Components/header";
+import FooterComponent from "../../Components/footer/footer";
 
 export default function AboutUs() {
   useEffect(() => {
-    document.title = 'CommuniTEA - About Us'
-  }, [])
+    document.title = "CommuniTEA - About Us";
+  }, []);
 
   return (
     <>
@@ -19,7 +19,11 @@ export default function AboutUs() {
           Meet our fantastic coding team from UCI's full-stack coding bootcamp!
         </p>
         <p>Stephanie:</p>
-        <p>Johnathan:</p>
+        <p>
+          Johnathan: Hello there! I'm Johnathan and my role during this project
+          mainly consisted of front-end design and user interface. I am training
+          to become a full-stack developer.
+        </p>
         <p>
           Darius: Hey, I'm Darius and this project I mainly focused on the
           front-end for this project. I'm looking to become a front-end dev in
@@ -33,5 +37,5 @@ export default function AboutUs() {
       </div>
       <FooterComponent />
     </>
-  )
+  );
 }
--- client/src/pages/SingleStore/singleStore.jsx
+++ client/src/pages/SingleStore/singleStore.jsx
@@ -1,37 +1,37 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/alt-text */
-import { useState, useEffect } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import FooterComponent from '../../Components/footer/footer'
-import HeaderComponent from '../../Components/header'
-import RecentReviewsContainer from '../../Components/recentReviews/recentReviewsContainer'
-import { StarOutlined } from '@ant-design/icons'
-import style from './singleStore.module.css'
-import { useQuery } from '@apollo/client'
-import { GET_STORE } from '../../utils/queries'
-import { useMutation } from '@apollo/client'
-import { FAV_STORE } from '../../utils/mutations'
-import ReviewForm from '../../Components/reviewForm/reviewForm'
+import { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import FooterComponent from "../../Components/footer/footer";
+import HeaderComponent from "../../Components/header";
+import RecentReviewsContainer from "../../Components/recentReviews/recentReviewsContainer";
+import { StarOutlined } from "@ant-design/icons";
+import style from "./singleStore.module.css";
+import { useQuery } from "@apollo/client";
+import { GET_STORE } from "../../utils/queries";
+import { useMutation } from "@apollo/client";
+import { FAV_STORE } from "../../utils/mutations";
+// import ReviewForm from '../../Components/reviewForm/reviewForm'
 
 export default function SingleStore() {
-  const store_id = useParams()
+  const store_id = useParams();
 
-  const location = useLocation()
-  const path = location.pathname.split('/')
-  const storeID = path[path.length - 1]
+  const location = useLocation();
+  const path = location.pathname.split("/");
+  const storeID = path[path.length - 1];
 
   // this is the query used to retrieve a store from db if needed
   // const { loading, data } = useQuery(GET_STORE(store_id));
   // const storeData = data?.getStore || {};
 
-  const [reviews, setReviews] = useState({})
-  const [storeData, setStoreData] = useState({})
+  const [reviews, setReviews] = useState({});
+  const [storeData, setStoreData] = useState({});
 
   useEffect(() => {
-    document.title = `CommuniTEA - ${storeData?.name}`
-    fetchYelpReviews()
-    fetchStoreDetails()
-  }, [])
+    document.title = `CommuniTEA - ${storeData?.name}`;
+    fetchYelpReviews();
+    fetchStoreDetails();
+  }, []);
 
   // --> I think we should just use reviews from out site right? <--
   // well depends, we can save the store to our database when a use clicks
@@ -39,49 +39,49 @@ export default function SingleStore() {
   // this page. but i also think its fine to only save store to db if user
   // interacts with it (review, react, favorites)...
 
-  const expressAPI = process.env.REACT_APP_API_ENDPOINT
+  const expressAPI = process.env.REACT_APP_API_ENDPOINT;
 
   // NOT WORKING YET
 
-  const [save, { error, data }] = useMutation(FAV_STORE)
+  const [save, { error, data }] = useMutation(FAV_STORE);
 
   const favoriteStore = async (e) => {
     // console.log("store data", storeData);
     try {
       const { saveData } = await save({
         variables: { store_id: storeData.id },
-      })
-      console.log(saveData)
+      });
+      console.log(saveData);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   // functions to fetch yelp reviews and single store information
   async function fetchYelpReviews() {
-    const endpoint = `/reviews/${storeID}`
-    const api = expressAPI + endpoint
+    const endpoint = `/reviews/${storeID}`;
+    const api = expressAPI + endpoint;
     await fetch(api, {
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => res.json())
-      .then((reviews) => setReviews(reviews))
+      .then((reviews) => setReviews(reviews));
   }
   async function fetchStoreDetails() {
-    const endpoint = `/store/${storeID}`
-    const api = expressAPI + endpoint
+    const endpoint = `/store/${storeID}`;
+    const api = expressAPI + endpoint;
     await fetch(api, {
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => res.json())
-      .then((stores) => setStoreData(stores))
+      .then((stores) => setStoreData(stores));
   }
 
   // if (loading) {
   //   ;<h2>Loading...</h2>
   // }
 
-  console.log(storeData.name)
+  console.log(storeData.name);
   return (
     <>
       {storeData && (
@@ -93,7 +93,7 @@ export default function SingleStore() {
             <div className={style.contentWrapper}>
               <header className={style.header}>
                 <h1>
-                  <a href={storeData.url} target='_blank'>
+                  <a href={storeData.url} target="_blank">
                     {storeData?.name}
                   </a>
                 </h1>
@@ -112,13 +112,13 @@ export default function SingleStore() {
                           <p key={key}>{category.alias}</p>
                         ))}
                       </div>
-                      <p id='rating' className={style.rating}>
+                      <p id="rating" className={style.rating}>
                         {storeData?.rating} <StarOutlined />
                       </p>
                       <p>{storeData?.location?.address1}</p>
                       <p>
-                        {storeData?.location?.city},{' '}
-                        {storeData?.location?.state}{' '}
+                        {storeData?.location?.city},{" "}
+                        {storeData?.location?.state}{" "}
                         {storeData?.location?.zip_code}
                       </p>
                       <p>price: {storeData?.price}</p>
@@ -142,11 +142,11 @@ export default function SingleStore() {
                 </section>
               </div>
               <article className={style.reviewFormArticle}>
-                <ReviewForm
+                {/* <ReviewForm
                   storeName={storeData?.name}
                   storeId={storeData?.id}
                   storeURL={storeData?.url}
-                />
+                /> */}
               </article>
             </div>
           </main>
@@ -154,5 +154,5 @@ export default function SingleStore() {
         </>
       )}
     </>
-  )
+  );
 }
